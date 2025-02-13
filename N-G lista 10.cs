f9b840dc221aef5fd7489ef9b17d90ed9855fba3
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-13 08:18:06 -0300

    Create N-G lista 10.cs

---
+++ N-G lista 10.cs
@@ -0,0 +1,34 @@
+using System;
+
+	  class Program
+  	{
+	  	public static void Main(string[] args)
+		  {
+		  	int[,] matriz = {
+
+                { 23, 87, 45, 12 },
+                { 56, 78, 91, 34 },
+                { 67, 10, 89, 42 },
+                { 99, 31, 74, 55 }
+            };
+      		
+			Console.WriteLine("Matriz 4x4:");
+      		for (int i = 0; i < 4; i++)
+      		  {
+          		  for (int j = 0; j < 4; j++)
+          		 {
+              		 if (matriz[i, j] % 2 == 0) 
+                    {
+                        Console.Write(matriz[i, j] + "\t");
+                    }
+                    else
+                    {
+                        Console.Write("-\t"); 
+                    }
+        	 	}
+            Console.WriteLine();
+      		  }
+      		Console.WriteLine("\nClique qualquer botão para encerrar...");
+      		Console.ReadKey();	
+	}	
+}
